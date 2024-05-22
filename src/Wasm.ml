7e438b2736f5f18f78d31a2c561715469a282184
Egor Kukoverov <me@bnt.to>
2024-05-22 06:34:26 +0300

    wasm: add globals

--- src/Wasm.ml
+++ src/Wasm.ml
@@ -1,8 +1,13 @@
 open SM
+open Language
 
-let compile_instr instr = match instr with
-  | CONST n -> [ Printf.sprintf "i32.const %d" n ]
-  | BINOP op -> (
+(* A set of strings *)
+module S = Set.Make (String)
+
+let compile_instr globals instr =
+  match instr with
+  | CONST n -> (globals, [ Printf.sprintf "i32.const %d" n ])
+  | BINOP op -> (globals, (
     match op with
       | "+" -> [ "i32.add" ]
       | "-" -> [ "i32.sub" ]
@@ -33,28 +38,45 @@ let compile_instr instr = match instr with
         "i32.ne"
       ]
       | _ -> Printf.printf "unsupported binop %s\n" op ; []
+    ))
+  | CALL (f, _, _) -> (globals, [ Printf.sprintf "call $%s" f ])
+  | LD x -> (
+    match x with
+    | Value.Global name -> (S.add ("$global_" ^ name) globals, ["global.get $global_" ^ name])
+    | x -> Printf.printf "unsupported memory %s\n" (GT.show Value.designation x) ; (globals, [])
     )
-  | CALL (f, _, _) -> [ Printf.sprintf "call $%s" f ]
-  | i -> Printf.printf "unsupported instr %s\n" (GT.show insn i) ; []
+  | ST x -> (
+    match x with
+    | Value.Global name -> (S.add ("$global_" ^ name) globals, ["global.set $global_" ^ name])
+    | x -> Printf.printf "unsupported memory %s\n" (GT.show Value.designation x) ; (globals, [])
+    )
+  | i -> Printf.printf "unsupported instr %s\n" (GT.show insn i) ; (globals, [])
 
-let rec compile instrs =
+let rec compile globals instrs =
   match instrs with
-    | [] -> []
+    | [] -> (globals, [])
     | instr :: instrs' -> (
-      let code = compile_instr instr in
-      let code' = compile instrs' in
-      code @ code'
+      let (globals', code) = compile_instr globals instr in
+      let (globals'', code') = compile globals' instrs' in
+      (globals'', code @ code')
     )
 
 let genwat cmd prog =
   let sm = SM.compile cmd prog in
-  let code = compile sm in
+  let globals = S.empty in
+  let (globals', code) = compile globals sm in
   let wat = Buffer.create 1024 in
   List.iter (fun instr -> Buffer.add_string wat (instr ^ "\n"))
   (
     [
       "(module";
-      "(import \"lama\" \"Lwrite\" (func $Lwrite (param i32)))";
+      "(import \"lama\" \"Lwrite\" (func $Lwrite (param i32)))"
+    ]
+    @ List.map
+      (fun name -> Printf.sprintf "(global %s (mut i32) (i32.const 0))" name)
+      (S.elements globals')
+    @
+    [
       "(func (export \"main\")"
     ]
     @ code @ [
