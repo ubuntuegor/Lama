5a9c6c55e9581aba67622a5f203e66631a034f1a
Egor Kukoverov <me@bnt.to>
2024-06-24 10:39:02 +0300

    create environment class, support ifs, enable tests

---
+++ run.js
@@ -0,0 +1,35 @@
+const fs = require("fs")
+const { argv } = require("process")
+
+const buffer = []
+
+function readNum() {
+    if (buffer.length == 0) {
+        const buf = Buffer.alloc(1024)
+        fs.readSync(0, buf)
+        buf.toString().split("\n").forEach(value => {
+            buffer.push(parseInt(value))
+        })
+    }
+
+    return buffer.shift()
+}
+
+const filePath = argv[2]
+const wasmFile = fs.readFileSync(filePath)
+WebAssembly.instantiate(wasmFile, {
+    "lama": {
+        "write": num => {
+            console.log(num)
+            return 0
+        },
+        "read": () => {
+            fs.writeSync(1, "> ")
+            return readNum()
+        }
+    }
+}).then(wasmModule => {
+    const instance = wasmModule.instance
+    const main = instance.exports.main
+    main()
+})
--- src/MyWasm.ml
+++ src/MyWasm.ml
@@ -1,121 +1,254 @@
 open Language
+module Wa = Wasm.Ast
+module Wt = Wasm.Types
+module M = Map.Make (String)
 
-(* A set of strings *)
-module S = Set.Make (String)
-module W = Wasm
+let phrase elem = Wasm.Source.(elem @@ no_region)
+let decode_s = Wasm.Utf8.decode
+let get_i32 i = Wasm.Value.I32 i
+let get_const i = Wa.Const (phrase @@ get_i32 @@ Int32.of_int i)
+let get_binary b = Wa.Binary (get_i32 b)
+let get_compare b = Wa.Compare (get_i32 b)
+let get_idx i = phrase @@ Int32.of_int i
 
-let phrase elem = W.Source.(elem @@ no_region)
-let decode_s = W.Utf8.decode
-let get_i32 i = W.Value.I32 i
-let get_const i = W.Ast.Const (phrase @@ get_i32 @@ Int32.of_int i)
-let get_binary b = W.Ast.Binary (get_i32 b)
-let get_compare b = W.Ast.Compare (get_i32 b)
-
-let rec compile_list ast =
+let rec compile_list env ast =
   match ast with
-  | Expr.Call (name, l) -> (
+  | Expr.Call (name, args) -> (
       match name with
       | Expr.Var name' ->
-          List.flatten (List.map compile_list l)
-          @ [ phrase @@ W.Ast.Call (phrase 0l) ]
-      | a ->
-          Printf.printf "unsupported call value %s\n" (GT.show Expr.t a);
-          [])
-  | Expr.Binop (op, lhs, rhs) -> (
-      compile_list lhs @ compile_list rhs
-      @
-      match op with
-      | "+" -> [ phrase @@ get_binary W.Ast.I32Op.Add ]
-      | "-" -> [ phrase @@ get_binary W.Ast.I32Op.Sub ]
-      | "*" -> [ phrase @@ get_binary W.Ast.I32Op.Mul ]
-      | "/" -> [ phrase @@ get_binary W.Ast.I32Op.DivS ]
-      | "%" -> [ phrase @@ get_binary W.Ast.I32Op.RemS ]
-      | "==" -> [ phrase @@ get_compare W.Ast.I32Op.Eq ]
-      | "!=" -> [ phrase @@ get_compare W.Ast.I32Op.Ne ]
-      | "<=" -> [ phrase @@ get_compare W.Ast.I32Op.LeS ]
-      | "<" -> [ phrase @@ get_compare W.Ast.I32Op.LtS ]
-      | ">=" -> [ phrase @@ get_compare W.Ast.I32Op.GeS ]
-      | ">" -> [ phrase @@ get_compare W.Ast.I32Op.GtS ]
-      | "&&" ->
-          [
-            phrase
-            @@ W.Ast.If
-                 ( W.Ast.VarBlockType (phrase 2l),
-                   [
-                     phrase @@ get_const 0; phrase @@ get_compare W.Ast.I32Op.Ne;
-                   ],
-                   [ phrase W.Ast.Drop; phrase @@ get_const 0 ] );
-          ]
-      | "!!" ->
-          [
-            phrase @@ get_binary W.Ast.I32Op.Or;
-            phrase @@ get_const 0;
-            phrase @@ get_compare W.Ast.I32Op.Ne;
-          ]
+          let index = env#get_func_by_name name' in
+          let env, code =
+            List.fold_left
+              (fun (env, acc) instr ->
+                let env, code = compile_list env instr in
+                (env, acc @ code))
+              (env, []) args
+          in
+          (env, code @ [ phrase @@ Wa.Call (get_idx index) ])
       | _ ->
-          Printf.printf "unsupported binop %s\n" op;
-          [])
-  | Expr.Const i -> [ phrase @@ get_const i ]
-  | Expr.Seq (s1, s2) -> compile_list s1 @ compile_list s2
-  | Expr.Ignore s -> compile_list s @ [ phrase W.Ast.Drop ]
-  | a ->
-      Printf.printf "unsupported structure %s\n" (GT.show Expr.t a);
-      []
+          report_error
+          @@ Printf.sprintf "unsupported call value %s\n" (GT.show Expr.t name))
+  | Expr.Binop (op, lhs, rhs) ->
+      let env, lhscode = compile_list env lhs in
+      let env, rhscode = compile_list env rhs in
+      let env, opcode =
+        match op with
+        | "+" -> (env, [ phrase @@ get_binary Wa.I32Op.Add ])
+        | "-" -> (env, [ phrase @@ get_binary Wa.I32Op.Sub ])
+        | "*" -> (env, [ phrase @@ get_binary Wa.I32Op.Mul ])
+        | "/" -> (env, [ phrase @@ get_binary Wa.I32Op.DivS ])
+        | "%" -> (env, [ phrase @@ get_binary Wa.I32Op.RemS ])
+        | "==" -> (env, [ phrase @@ get_compare Wa.I32Op.Eq ])
+        | "!=" -> (env, [ phrase @@ get_compare Wa.I32Op.Ne ])
+        | "<=" -> (env, [ phrase @@ get_compare Wa.I32Op.LeS ])
+        | "<" -> (env, [ phrase @@ get_compare Wa.I32Op.LtS ])
+        | ">=" -> (env, [ phrase @@ get_compare Wa.I32Op.GeS ])
+        | ">" -> (env, [ phrase @@ get_compare Wa.I32Op.GtS ])
+        | "&&" ->
+            let index, env =
+              env#type_upsert
+                Wt.(
+                  RecT
+                    [
+                      SubT
+                        ( Final,
+                          [],
+                          DefFuncT (FuncT ([ NumT I32T ], [ NumT I32T ])) );
+                    ])
+            in
+            ( env,
+              [
+                phrase
+                @@ Wa.If
+                     ( Wa.VarBlockType (get_idx index),
+                       [
+                         phrase @@ get_const 0;
+                         phrase @@ get_compare Wa.I32Op.Ne;
+                       ],
+                       [ phrase Wa.Drop; phrase @@ get_const 0 ] );
+              ] )
+        | "!!" ->
+            ( env,
+              [
+                phrase @@ get_binary Wa.I32Op.Or;
+                phrase @@ get_const 0;
+                phrase @@ get_compare Wa.I32Op.Ne;
+              ] )
+        | _ -> report_error @@ Printf.sprintf "unsupported binop %s\n" op
+      in
+      (env, lhscode @ rhscode @ opcode)
+  | Expr.Const i -> (env, [ phrase @@ get_const i ])
+  | Expr.Seq (s1, s2) ->
+      let env, code1 = compile_list env s1 in
+      let env, code2 = compile_list env s2 in
+      (env, code1 @ code2)
+  | Expr.Ignore s ->
+      let env, code = compile_list env s in
+      (env, code @ [ phrase Wa.Drop ])
+  | Expr.Var name ->
+      let index = env#get_global name in
+      (env, [ phrase @@ Wa.GlobalGet (get_idx index) ])
+  | Expr.Assign (Expr.Ref name, instr) ->
+      let index = env#get_global name in
+      let env, code = compile_list env instr in
+      (env, code @ [ phrase @@ Wa.GlobalSet (get_idx index) ; phrase @@ Wa.GlobalGet (get_idx index) ])
+  | Expr.If (c, s1, s2) ->
+      let env, c_code = compile_list env c in
+      let env, s1_code = compile_list env s1 in
+      let env, s2_code = compile_list env s2 in
+      ( env,
+        c_code
+        @ [
+            phrase @@ Wa.If (ValBlockType (Some (NumT I32T)), s1_code, s2_code);
+          ] )
+  | Expr.Scope ([], instr) -> compile_list env instr
+  | _ ->
+      report_error
+      @@ Printf.sprintf "unsupported structure %s\n" (GT.show Expr.t ast)
+
+class env =
+  object (self)
+    val types =
+      object (self)
+        val index = 0
+        val types_rev = []
+
+        method upsert t =
+          match List.assoc_opt t types_rev with
+          | None ->
+              (index, {<index = index + 1; types_rev = (t, index) :: types_rev>})
+          | Some i -> (i, self)
+
+        method get_types = List.rev @@ List.map fst types_rev
+      end
+
+    val imports_rev = []
+    val func_index = 0
+    val funcs_rev = []
+    val func_by_name = M.empty
+    val exports = []
+    val globals_index = 0
+    val globals_rev = []
+    val globals_by_name = M.empty
+
+    method type_upsert t =
+      let index, types = types#upsert t in
+      (index, {<types>})
+
+    method add_function_import name t =
+      let rec_type = Wt.(RecT [ SubT (Final, [], DefFuncT t) ]) in
+      let type_index, types = types#upsert rec_type in
+      let new_import =
+        Wa.
+          {
+            module_name = decode_s "lama";
+            item_name = decode_s name;
+            idesc = phrase @@ Wa.FuncImport (get_idx type_index);
+          }
+      in
+      {<types
+       ; imports_rev = new_import :: imports_rev
+       ; func_index = func_index + 1
+       ; func_by_name = func_by_name |> M.add name func_index>}
+
+    method add_function name t body =
+      let rec_type = Wt.(RecT [ SubT (Final, [], DefFuncT t) ]) in
+      let type_index, types = types#upsert rec_type in
+      {<types
+       ; funcs_rev = Wa.{ ftype = get_idx type_index; locals = []; body }
+                     :: funcs_rev
+       ; func_index = func_index + 1
+       ; func_by_name = func_by_name |> M.add name func_index>}
+
+    method export_function name =
+      let index = M.find name func_by_name in
+      {<exports = Wa.
+                    {
+                      name = decode_s name;
+                      edesc = phrase @@ FuncExport (get_idx index);
+                    }
+                  :: exports>}
+
+    method get_func_by_name name =
+      try M.find name func_by_name
+      with Not_found ->
+        report_error
+        @@ Printf.sprintf "unable to find function with name \"%s\"" name
+
+    method add_global name =
+      if M.mem name globals_by_name then
+        report_error
+        @@ Printf.sprintf "name \"%s\" is already defined in the scope" name
+      else
+        {<globals_index = globals_index + 1
+         ; globals_rev = Wa.
+                           {
+                             gtype = Wt.(GlobalT (Var, NumT I32T));
+                             ginit = phrase @@ [ phrase @@ get_const 0 ];
+                           }
+                         :: globals_rev
+         ; globals_by_name = globals_by_name |> M.add name globals_index>}
+
+    method get_global name =
+      try M.find name globals_by_name
+      with Not_found ->
+        report_error
+        @@ Printf.sprintf "unable to find global with name \"%s\"" name
+
+    method get_module =
+      phrase
+        Wa.
+          {
+            empty_module with
+            imports = List.rev @@ List.map phrase imports_rev;
+            funcs = List.rev @@ List.map phrase funcs_rev;
+            types = List.map phrase types#get_types;
+            exports = List.map phrase exports;
+            globals = List.rev @@ List.map phrase globals_rev;
+          }
+  end
 
 let compile ast =
+  let env = new env in
   match ast with
-  | Expr.Scope (_, i) ->
-      let types =
-        [
-          (phrase
-          @@ W.Types.(RecT [ SubT (Final, [], DefFuncT (FuncT ([], []))) ]));
-          (phrase
-          @@ W.Types.(
-               RecT [ SubT (Final, [], DefFuncT (FuncT ([ NumT I32T ], []))) ])
-          );
-          (phrase
-          @@ W.Types.(
-               RecT
-                 [
-                   SubT
-                     (Final, [], DefFuncT (FuncT ([ NumT I32T ], [ NumT I32T ])));
-                 ]));
-        ]
+  | Expr.Scope (decls, instr) ->
+      let env =
+        env#add_function_import "write" (FuncT ([ NumT I32T ], [ NumT I32T ]))
       in
-      let imports =
-        [
-          phrase
-            W.Ast.
-              {
-                module_name = decode_s "lama";
-                item_name = decode_s "Lwrite";
-                idesc = phrase @@ W.Ast.FuncImport (phrase 1l);
-              };
-        ]
+      let env = env#add_function_import "read" (FuncT ([], [ NumT I32T ])) in
+      let globals =
+        List.filter_map
+          (fun (name, (_, d)) ->
+            match d with `Variable init -> Some (name, init) | _ -> None)
+          decls
       in
-      let funcs =
-        [
-          phrase W.Ast.{ ftype = phrase 0l; locals = []; body = compile_list i };
-        ]
+      let env =
+        List.fold_left (fun env (name, _) -> env#add_global name) env @@ globals
       in
-      let exports =
-        [
-          phrase
-            W.Ast.
-              {
-                name = decode_s "main";
-                edesc = phrase @@ FuncExport (phrase 1l);
-              };
-        ]
+      let env, init =
+        List.fold_left
+          (fun (env, acc) (name, instr) ->
+            let env, code = compile_list env instr in
+            ( env,
+              acc @ code
+              @ [ phrase @@ Wa.GlobalSet (get_idx @@ env#get_global name) ] ))
+          (env, [])
+          (List.filter_map
+             (fun (name, init) ->
+               match init with Some i -> Some (name, i) | _ -> None)
+             globals)
       in
-      phrase W.Ast.{ empty_module with imports; funcs; types; exports }
+      let env, code = compile_list env (Expr.Ignore instr) in
+      let env = env#add_function "main" (FuncT ([], [])) (init @ code) in
+      let env = env#export_function "main" in
+      env#get_module
   | _ -> report_error "expected root scope"
 
-let genast cmd ((imports, _), p) = compile p
+let genast _ ((imports, _), p) = compile p
 
 let build cmd prog =
   let module' = genast cmd prog in
   let oc = open_out (cmd#basename ^ ".wast") in
-  W.Print.module_ oc 80 module';
+  Wasm.Print.module_ oc 80 module';
   close_out oc;
-  cmd#dump_file "wasm" (W.Encode.encode module')
+  cmd#dump_file "wasm" (Wasm.Encode.encode module')
